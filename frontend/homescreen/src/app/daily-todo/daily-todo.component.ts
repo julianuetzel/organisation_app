7d409aec4484598923fe2f768776769dc3540cff
julianuetzel <71690925+julianuetzel@users.noreply.github.com>
2022-11-19 22:00:34 +0100

    Done

--- frontend/homescreen/src/app/daily-todo/daily-todo.component.ts
+++ frontend/homescreen/src/app/daily-todo/daily-todo.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 import { DailyTodoService } from '../dailytodo.service';
 import { DailyToDo, DailyToDoUpdate } from './daily-todo';
+import { DialogComponent } from './dialog/dialog.component'
+import { MatDialog } from '@angular/material/dialog';
 
 
 
@@ -12,19 +14,18 @@ import { DailyToDo, DailyToDoUpdate } from './daily-todo';
 })
 export class DailyTodoComponent implements OnInit {
   date = new Date();
+  print_date = this.date.toLocaleDateString();
   daily_todo: DailyToDo | undefined;
   daily_todos: DailyToDo[] = [];
   mode: ProgressSpinnerMode = 'determinate';
 
-  constructor(private dailytodoService: DailyTodoService) { }
+  constructor(
+    private dailytodoService: DailyTodoService, 
+    public dialog: MatDialog
+    ) { }
   
   ngOnInit(): void {
     this.getDailyTodosByDate(this.formatDate(this.date));
-    console.log(this.formatDate(this.date))
-  }
-
-  date_today(): string {
-    return this.date.toLocaleDateString()
   }
 
   formatDate(date: Date) {
@@ -54,62 +55,59 @@ export class DailyTodoComponent implements OnInit {
   }
   
   getDailyTodos(): DailyToDo[] {
-    this.dailytodoService.get_by_date(this.date_today()).subscribe(daily_todos => this.daily_todos = daily_todos);
+    this.dailytodoService.get_by_date(this.formatDate(this.date)).subscribe(daily_todos => this.daily_todos = daily_todos);
     return this.daily_todos
   }
 
   addDailyTodo(task: string): void {
     task = task.trim(); 
-    if (task) {
-      let new_daily_todo : DailyToDo = {
-        id: "",
-        task: task,
-        done: false,
-        task_date: this.date_today(),
-      }; 
+    if (!task) { return; }
 
-      this.dailytodoService.create( new_daily_todo )
-        .subscribe(daily_todo => {
-          this.daily_todos.push(daily_todo);
-        });
-      }
+    let new_daily_todo : DailyToDo = {
+      _id: "",
+      task: task,
+      done: false,
+      task_date: this.formatDate(this.date),
+    }; 
+
+    this.dailytodoService.create( new_daily_todo )
+      .subscribe(daily_todo => {
+        this.daily_todos.push(daily_todo);
+      });
   }
 
-  updateDailyTodoStatus(daily_todo: DailyToDo): void {
-    var updated_daily_todo = this.daily_todos.find(d_todo => d_todo.id == daily_todo.id)
-
-    if (updated_daily_todo !== undefined) {
-
-      let updated_daily_todo : DailyToDoUpdate = {
-        task: daily_todo.task,
-        done: !daily_todo.done,
-      }
-
-      this.dailytodoService.update(daily_todo.id, updated_daily_todo)
-        .subscribe(daily_todo => {
-          this.daily_todos.push(daily_todo)
-        })
-      this.daily_todos.filter(d_todo => d_todo.id == daily_todo.id).pop()
+  updateStatus(daily_todo: DailyToDo): void {
+    var new_status = !daily_todo.done;
+    let updated_daily_todo : DailyToDoUpdate = {
+      task: daily_todo.task,
+      done: new_status,
     }
+    this.dailytodoService.update(daily_todo, updated_daily_todo).subscribe() ;
   }
 
-  updateDailyTodoTask(daily_todo: DailyToDo): void {
-    if (this.daily_todo) {
-      let updated_daily_todo : DailyToDoUpdate = {
-        task: daily_todo.task,
-        done: daily_todo.done,
-      };
-
-      this.daily_todos = this.daily_todos.filter(d_todo => d_todo !== daily_todo);
-      this.dailytodoService.update(daily_todo.id, updated_daily_todo)
-        .subscribe()
+  updateDailyTodo(daily_todo: DailyToDo, new_task: string): void {
+    let updated_daily_todo : DailyToDoUpdate = {
+      task: new_task,
+      done: daily_todo.done,
     }
+    this.dailytodoService.update(daily_todo, updated_daily_todo).subscribe() ;
   }
 
   deleteDailyTodo(daily_todo: DailyToDo): void {
     this.daily_todos = this.daily_todos.filter(d_todo => d_todo !== daily_todo);
-    this.dailytodoService.delete(daily_todo.id)
+    this.dailytodoService.delete(daily_todo._id)
       .subscribe()
+  }
+
+  openDialog(daily_todo: DailyToDo): void{
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: daily_todo.task,
+    })
+    dialogRef.afterClosed().subscribe(result => {
+      daily_todo.task=result;
+      this.updateDailyTodo(daily_todo, daily_todo.task)
+    });
   }
 
   progress(): number{
@@ -118,5 +116,21 @@ export class DailyTodoComponent implements OnInit {
       if (daily_todo.done) i++;
     }
     return (i*100/this.daily_todos.length)    
+  }
+
+  day_before(date: Date): void {   
+    var bday = new Date(date);
+    this.date = new Date(bday.setDate(bday.getDate() - 1));
+    this.print_date = this.date.toLocaleDateString();
+    console.log(this.date)
+    this.getDailyTodosByDate(this.formatDate(this.date))
+  }
+
+  day_after(date: Date): void {
+    var nday = new Date(date);
+    this.date = new Date(nday.setDate(nday.getDate() + 1));
+    this.print_date = this.date.toLocaleDateString();
+    console.log(this.date)
+    this.getDailyTodosByDate(this.formatDate(this.date))
   }
 }
