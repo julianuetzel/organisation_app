145cded228283ba360403d70bf0f334fadaf7b09
julianuetzel <71690925+julianuetzel@users.noreply.github.com>
2023-01-10 14:31:44 +0100

    Frontend done

---
+++ frontend/homescreen/src/app/finances/dialog/dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+interface Data{
+  name: string,
+  amount: number
+}
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Data,
+  ) {}
+
+  abbort(): void {
+    this.dialogRef.close();
+  }
+}
